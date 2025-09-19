7fb8afeb1a6466ac88af2b7e2122c402bfe74586
Adiii2004 <adityaprofessional108@gmail.com>
2025-09-19 21:57:42 +0530

    Add files via upload

---
+++ Core2Web_java-main/JAVA/10.DataTypes/Daily Codes/Datatypes/short/short2.java
@@ -0,0 +1,25 @@
+class Short2 {
+
+	public static void main (String[] args ) {
+
+		short sh1 = 'A';
+
+		short sh2 = '1';
+
+		System.out.println(sh1);
+
+		System.out.println(sh2);
+
+	}
+
+}		
+
+
+
+
+/*
+Output
+65
+49
+ here ascii values are get printed cause we use '' for values and we use short 
+
---
+++ Core2Web_java-main/JAVA/10.DataTypes/Daily Codes/Datatypes/tabButton/tab.java
@@ -0,0 +1,11 @@
+  
+
+class tab  {
+
+	public static void main (String[] args)  {
+
+		System.out.println("Aditya\tChandrakant\nSalunke");
+
+	}
+
+}
